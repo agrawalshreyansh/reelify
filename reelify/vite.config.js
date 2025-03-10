fb5782e229ffa6abbf5b3820f5ffa0c1927f8cdd
shreyansh <shreyansh3536@gmail.com>
2025-03-10 22:03:06 +0530

    V1.1.4.2

--- reelify/vite.config.js
+++ reelify/vite.config.js
@@ -1,15 +1,4 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
 
-
-// export default defineConfig({
-//   base: '/',
-//   plugins: [
-//     tailwindcss(),
-//     react()
-//   ],
-// })
 
 
 import { defineConfig } from 'vite';
