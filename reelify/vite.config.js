24c0e3873ce58a664448558d7153460a4cf288e5
shreyansh <shreyansh3536@gmail.com>
2025-03-10 22:36:21 +0530

    V1.1.4.2

--- reelify/vite.config.js
+++ reelify/vite.config.js
@@ -7,8 +7,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  build: {
-    outDir: 'dist', // Default, ensure it's correct
-  },
-  base: "/", // Prevents broken paths on Vercel
+  base: "/", 
 });
