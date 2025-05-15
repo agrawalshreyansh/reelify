1872d006fc7a964b6f75808bc8806f96f6af8418
Shreyansh Agrawal <shreyanshagrawal@192.168.254.16>
2025-05-15 14:14:14 +0545

    api name fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 
-import ApiService from './apiService.js';
+import ApiService from './ApiService.js';
 
 export const fetchData = (endpoint, withCredentials = false) => ApiService.fetchData(endpoint, withCredentials);
 export const postData = (endpoint, body, withCredentials = false) => ApiService.postData(endpoint, body, withCredentials);
